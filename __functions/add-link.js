04dbadfdb9bff648bd2448025e73dea584cdb4a8
Simon McManus <mcmanus.simon@gmail.com>
2022-03-01 13:32:39 +0000

    ws

--- __functions/add-link.js
+++ __functions/add-link.js
@@ -24,7 +24,6 @@ exports.handler = async(event) => {
             return { statusCode: 400, body: 'no-url' }
         }
 
-
         var params = {
             Key: "links.json",
             Bucket: 'netlify-files',
