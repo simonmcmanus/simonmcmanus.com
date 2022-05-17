f9925f07f0fd21ef331f4487d8c5bb1fdc66df6e
Simon McManus <mcmanus.simon@gmail.com>
2022-05-17 07:48:27 +0100

    add tags to posts

--- _includes/layouts/mappers/post.js
+++ _includes/layouts/mappers/post.js
@@ -1,4 +1,6 @@
+const { dateLink, tagList } = require('./links')
 module.exports = (data) => ({
     title: `${data.title} by Simon McManus`,
+    '.tag': tagList(data.tags),
     "meta[name=keywords]": { content: data.tags.join(', ') }
 })
