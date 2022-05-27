73e416d4372949fa07e5fc343ac4f7a24fae8c5c
Simon McManus <mcmanus.simon@gmail.com>
2022-05-27 20:04:45 +0100

    post descriptions

--- _includes/layouts/mappers/post.js
+++ _includes/layouts/mappers/post.js
@@ -1,6 +1,9 @@
 const { dateLink, tagList } = require('./links')
-module.exports = (data) => ({
-    title: `${data.title} by Simon McManus`,
-    '.tag': tagList(data.tags),
-    "meta[name=keywords]": { content: data.tags.join(', ') }
-})
+module.exports = (data) => {
+    return {
+        title: `${data.title} by Simon McManus`,
+        '.tag': tagList(data.tags),
+        "meta[name=description]": { content: `${data.title} by Simon McManus` },
+        "meta[name=keywords]": { content: data.tags.join(', ') }
+    }
+}
