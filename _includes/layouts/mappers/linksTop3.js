682ae165851a4c593da86328f00ea630042c3ef5
Simon McManus <mcmanus.simon@gmail.com>
2022-10-20 07:10:10 +0100

    order of links on homepage

--- _includes/layouts/mappers/linksTop3.js
+++ _includes/layouts/mappers/linksTop3.js
@@ -5,6 +5,6 @@ const link = require('./link')
 module.exports = (data) => {
     const numberToShow = data.showRecent || 3
     return {
-        '.recent-links article': data.collections.links.reverse().slice(-numberToShow).map(link)
+        '.recent-links article': data.collections.links.reverse().slice(-numberToShow).reverse().map(link)
     }
 }
