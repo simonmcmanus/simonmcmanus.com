9ce074c5afc4799be20c3f0780c550cbb0d38503
Simon McManus <mcmanus.simon@gmail.com>
2024-05-27 20:55:35 +0100

    generate-cv

---
+++ __functions/generate-cv.js
@@ -0,0 +1,25 @@
+exports.handler = async(event) => {
+
+    if (event.headers['x-api-key'] !== process.env.API_KEY) {
+        return { statusCode: 404 }
+    }
+
+
+    try {
+
+        const { skills, strengths, summary } = JSON.parse(event.body)
+
+
+        if (skills === '' || strengths === '' || summary === '') {
+            console.log('error: not enough data')
+            return { statusCode: 400, body: 'no-url' }
+        }
+
+        console.log('..', skills, strengths, summary)
+        return { statusCode: 200, body: 'done' }
+
+    } catch (e) {
+        console.log(e)
+        return { statusCode: 500, body: e.message }
+    }
+}
