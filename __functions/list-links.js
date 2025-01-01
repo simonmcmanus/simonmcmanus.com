9475a68b3e8153d5ed0af022c0b8874b84b2f848
Simon McManus <mcmanus.simon@gmail.com>
2025-01-01 09:01:44 +0000

    test

--- __functions/list-links.js
+++ __functions/list-links.js
@@ -3,6 +3,7 @@ const storage = require('./storage');
 exports.handler = async(event, context) => {
     try {
         const links = await storage.get('links.json')
+        console.log('links', links)
         return {
             statusCode: 200,
             body: links,
