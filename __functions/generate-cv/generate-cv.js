8bd737d3e991f643be28352b64c7d578f303bc61
Simon McManus <mcmanus.simon@gmail.com>
2025-01-26 17:04:48 +0000

    cv import tweaks

--- __functions/generate-cv/generate-cv.js
+++ __functions/generate-cv/generate-cv.js
@@ -1,8 +1,6 @@
 
 const sizlate = require('sizlate');
-const request = require('superagent');
-
-
+const markup = fs.readFileSync(require.resolve('../../static/cv.html'), { encoding: 'utf8' });
 exports.handler = async(event) => {
     if (event.headers['x-api-key'] !== process.env.API_KEY) {
         return { statusCode: 404 }
@@ -10,9 +8,8 @@ exports.handler = async(event) => {
     try {
         const { skills, strengths, summary } = JSON.parse(event.body)
 
-        const cv = await request.get('https://simonmcmanus.com/cv')
-
-        const body = sizlate.render(cv.text, {
+        console.log(JSON.parse(event.body))
+        const body = sizlate.render(markup, {
             '.profile': summary || '',
             '[data-strengths]': '<li></li>',
             '[data-strengths] li': strengths.split(',').map((a) => a.trim()) || [],
