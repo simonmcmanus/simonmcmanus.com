2d8356fc7014e747a08fb6e0e63bf24b4baca3eb
Simon McManus <mcmanus.simon@gmail.com>
2025-01-26 13:51:42 +0000

    adds a log

--- __functions/generate-cv/generate-cv.js
+++ __functions/generate-cv/generate-cv.js
@@ -1,46 +1,22 @@
 const fs = require('fs');
 const sizlate = require('sizlate');
 const markup = fs.readFileSync(require.resolve('./cv1.html'), { encoding: 'utf8' });
-
-
-
 exports.handler = async(event) => {
-    console.log('incoming')
-
     if (event.headers['x-api-key'] !== process.env.API_KEY) {
         return { statusCode: 404 }
     }
-
-
-
-    console.log('ap8 ok')
-
-
     try {
-
         const { skills, strengths, summary } = JSON.parse(event.body)
-        const skillsArr = skills.split(',').map((a) => a.trim())
-        const strengthsArr = strengths.split(',').map((a) => a.trim())
 
-
-        // if (skills === '' || strengths === '' || summary === '') {
-        //     console.log('error: not enough data')
-        //     return { statusCode: 400, body: 'no-url' }
-        // }
-
-        const rendered = sizlate.render(markup, {
+        console.log(JSON.parse(event.body))
+        const body = sizlate.render(markup, {
             '.profile': summary || '',
             '[data-strengths]': '<li></li>',
-            '[data-strengths] li': skillsArr || [],
+            '[data-strengths] li': skills.split(',').map((a) => a.trim()) || [],
             '[data-technology]': '<li></li>',
-            '[data-technology] li': strengthsArr || [],
-
+            '[data-technology] li': strengths.split(',').map((a) => a.trim()) || [],
         })
-        console.log('..', skillsArr)
-        console.log('..', strengthsArr)
-        console.log('..', summary)
-        return { statusCode: 200, body: rendered }
-
+        return { statusCode: 200, body }
     } catch (e) {
         console.log(e)
         return { statusCode: 500, body: e.message }
