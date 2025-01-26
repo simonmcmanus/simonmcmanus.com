3cffb11942aea33a1252aedd943afb46377a4892
Simon McManus <mcmanus.simon@gmail.com>
2025-01-26 16:05:49 +0000

    recruiter tweaks

--- __functions/cv-ai.js
+++ __functions/cv-ai.js
@@ -30,7 +30,7 @@ exports.getMeta = async({ url, markup }) => {
             },
             {
               role: 'user',
-              content: `based on the job description, and the content of my CV generate a plain text covering letter to the recruiter explaining why im the perfect application, return the text in a cover property of the json.  `
+              content: `based on the job description, and the content of my CV generate a covering letter to the recruiter explaining why im the perfect applicant, return the text in a cover property of the json. Ensure the message is ready to copy and paste and send straight to the recuriter `
             },
             {
               role: 'user',
