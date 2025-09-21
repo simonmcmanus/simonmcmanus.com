48e8ffbd532da9fe7d935f5c45bdb77657fb36f4
Simon McManus <mcmanus.simon@gmail.com>
2025-09-21 08:22:02 +0100

    add to notes file

--- __functions/upload-image.js
+++ __functions/upload-image.js
@@ -1,12 +1,19 @@
 const AWS = require("aws-sdk");
 const { Buffer } = require("buffer");
+const storage = require('./storage.js')
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
 });
 
+
+
 exports.handler = async (event, context) => {
+
+      if (event.headers['x-api-key'] !== process.env.API_KEY) {
+        return { statusCode: 404 }
+    }
   if (event.httpMethod !== "POST") {
     return {
       statusCode: 405,
@@ -14,6 +21,8 @@ exports.handler = async (event, context) => {
     };
   }
 
+  const notes = await storage.get('notes.json')
+  
   try {
     // Netlify passes the raw base64 body by default
     const body = Buffer.from(event.body, event.isBase64Encoded ? "base64" : "utf8");
@@ -31,13 +40,28 @@ exports.handler = async (event, context) => {
 
     const response = await s3.upload(params).promise();
     console.log('res', response)
+    const url = `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${filename}`
+
+
+    const note = {
+      created: new Date(),
+      image: 'https://netlify-files.s3.amazonaws.com/upload-1758437037577.jpg',
+      title: 'Don`t be an idiot'
+      event: 'FFConf 2025',
+      speaker: 'Asim Hussain',
+      tags: 'idiot, ffconf',
+      url
+    }
+
+    notes.push(note)
+    await storage.put('notes.json', notes)
 
     return {
       statusCode: 200,
       body: JSON.stringify({
         message: "Upload successful",
         key: filename,
-        url: `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${filename}`,
+        url,
       }),
     };
   } catch (error) {
