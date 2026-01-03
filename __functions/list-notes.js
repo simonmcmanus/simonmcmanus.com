602e748cf061756034152fc34bc1490efb6fba14
Simon McManus <mcmanus.simon@gmail.com>
2026-01-03 12:48:41 +0000

    dont use new

--- __functions/list-notes.js
+++ __functions/list-notes.js
@@ -6,8 +6,8 @@ export default async(event, context) => {
     try {
         const notes = await storage.get('notes.json')
         console.log('m', notes)
-         return new Response.json(notes);
+         return Response.json(notes);
     } catch (e) {
-        return new Response(e.message, { status: 500 });
+        return Response(e.message, { status: 500 });
     }
 }
